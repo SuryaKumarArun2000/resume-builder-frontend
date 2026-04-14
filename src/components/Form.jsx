53df9394d3bd15f1a36ca4a8fb2f764ca221b5cb
Surya Kumar Arun <surya.arun@newgensoft.com>
2026-04-14 17:58:09 +0530

    fix formjsx getResume to get Resumes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -223,7 +223,7 @@ const loadResume = async (id) => {
 
 const fetchResumes = async () => {
   try {
-    const res = await fetch("https://resume-builder-backend-0ij6.onrender.com/getResume");
+    const res = await fetch("https://resume-builder-backend-0ij6.onrender.com/getResumes");
     const data = await res.json();
     setResumeList(data);
   } catch (err) {
